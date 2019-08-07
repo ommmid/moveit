732cfa9b736fa6055bd7fad04ac5f373fb76b998
omid1366 <omid@picknik.ai>
2019-08-07 12:12:49 -0600

    broke the dependency to tesseract and trajoopt_ros

---
+++ moveit_planners/trajopt/trajopt/include/trajopt/kinematic_terms.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <moveit/planning_scene/planning_scene.h>
+
+#include <Eigen/Geometry>
+
+namespace trajopt
+{
+/**
+ * @brief Extracts the vector part of quaternion
+ */
+inline Eigen::Vector3d rotVec(const Eigen::Matrix3d& m)
+{
+  Eigen::Quaterniond q;
+  q = m;
+  return Eigen::Vector3d(q.x(), q.y(), q.z());
+}
+
+/**
+ * @brief Used to calculate the error for StaticCartPoseTermInfo
+ * This is converted to a cost or constraint using TrajOptCostFromErrFunc or TrajOptConstraintFromErrFunc
+ */
+struct CartPoseErrCalculator : public sco::VectorOfVector
+{
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  Eigen::Isometry3d target_pose_inv_;
+  planning_scene::PlanningSceneConstPtr planning_scene_;
+  std::string link_;
+  Eigen::Isometry3d tcp_;
+
+  CartPoseErrCalculator(const Eigen::Isometry3d& pose, const planning_scene::PlanningSceneConstPtr planning_scene,
+                        const std::string& link, Eigen::Isometry3d tcp = Eigen::Isometry3d::Identity())
+    : target_pose_inv_(pose.inverse()), planning_scene_(planning_scene), link_(link), tcp_(tcp)
+  {
+  }
+
+  Eigen::VectorXd operator()(const Eigen::VectorXd& dof_vals) const override;
+};
+
+// TODO(omid): The following should be added and adjusted from trajopt
+// JointPosEqCost
+// JointPosIneqCost
+// JointPosEqConstraint
+// JointPosIneqConstraint
+
+struct JointVelErrCalculator : sco::VectorOfVector
+{
+  /** @brief Velocity target */
+  double target_;
+  /** @brief Upper tolerance */
+  double upper_tol_;
+  /** @brief Lower tolerance */
+  double lower_tol_;
+  JointVelErrCalculator() : target_(0.0), upper_tol_(0.0), lower_tol_(0.0)
+  {
+  }
+  JointVelErrCalculator(double target, double upper_tol, double lower_tol)
+    : target_(target), upper_tol_(upper_tol), lower_tol_(lower_tol)
+  {
+  }
+
+  Eigen::VectorXd operator()(const Eigen::VectorXd& var_vals) const;
+};
+
+struct JointVelJacobianCalculator : sco::MatrixOfVector
+{
+  Eigen::MatrixXd operator()(const Eigen::VectorXd& var_vals) const;
+};
+
+}  // namespace trajopt
